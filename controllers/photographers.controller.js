215ba98c46856f5dd7652801abfb208b00fc7d13
Wes Reid <bwreid@gmail.com>
2021-12-21 16:12:22 -0800

    Add remaining CRUD routes for photographers

--- controllers/photographers.controller.js
+++ controllers/photographers.controller.js
@@ -1,5 +1,80 @@
 const photographerModel = require("../models/photographers.model");
 
+async function getAll(_req, res, next) {
+  try {
+    const payload = await photographerModel.getAll();
+    res.locals = {
+      status: 200,
+      payload,
+    };
+    next();
+  } catch (error) {
+    next({
+      errors: [error],
+    });
+  }
+}
+
+function getOne(_req, res, next) {
+  try {
+    res.locals = {
+      status: 200,
+      payload: res.locals.payload,
+    };
+    next();
+  } catch (error) {
+    next({
+      errors: [error],
+    });
+  }
+}
+
+async function create(req, res, next) {
+  try {
+    const payload = await photographerModel.create(req.body);
+    res.locals = {
+      status: 201,
+      payload,
+    };
+    next();
+  } catch (error) {
+    next({
+      errors: [error],
+    });
+  }
+}
+
+async function update(req, res, next) {
+  try {
+    const [payload] = await photographerModel.update(req.body);
+    res.locals = {
+      status: 200,
+      payload,
+    };
+    next();
+  } catch (error) {
+    next({
+      errors: [error],
+    });
+  }
+}
+
+async function destroy(req, res, next) {
+  try {
+    const [payload] = await photographerModel.destroy(res.locals.payload.id);
+    res.locals = {
+      status: 200,
+      payload,
+    };
+    next();
+  } catch (error) {
+    next({
+      errors: [error],
+    });
+  }
+}
+
+// Helpers
 function validate(req, _res, next) {
   const { preferred_name, surname, unsplash_profile_url } = req.body;
 
@@ -20,19 +95,27 @@ function validate(req, _res, next) {
   next();
 }
 
-async function create(req, res, next) {
-  try {
-    const payload = await photographerModel.create(req.body);
-    res.locals = {
-      status: 201,
-      payload,
-    };
-    next();
-  } catch (error) {
-    next({
-      errors: [error],
+async function findById(req, res, next) {
+  const id = req.params.id;
+  const [payload] = await photographerModel.getOne(id);
+
+  if (!payload) {
+    return next({
+      status: 404,
+      errors: [`Photographer with ID of ${id} could not be found.`],
     });
   }
+
+  res.locals.payload = payload;
+  next();
 }
 
-module.exports = { validate, create };
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy,
+  validate,
+  findById,
+};
--- models/photographers.model.js
+++ models/photographers.model.js
@@ -1,4 +1,22 @@
-const db = require("../db/config");
+const db = require("../db");
+
+async function getAll() {
+  try {
+    return await db.any(`SELECT * FROM photographers`);
+  } catch (error) {
+    console.error(error);
+    throw "Internal server error.";
+  }
+}
+
+async function getOne(id) {
+  try {
+    return await db.any(`SELECT * FROM photographers WHERE id=$1`, [id]);
+  } catch (error) {
+    console.error(error);
+    throw "Internal server error.";
+  }
+}
 
 async function create({ preferred_name, surname, unsplash_profile_url }) {
   try {
@@ -18,4 +36,51 @@ async function create({ preferred_name, surname, unsplash_profile_url }) {
   }
 }
 
-module.exports = { create };
+async function update(photographer) {
+  try {
+    return await db.any(
+      `
+      UPDATE
+        photographers
+      SET
+        preferred_name=$<preferred_name>,
+        surname=$<surname>,
+        unsplash_profile_url=$<unsplash_profile_url>
+      WHERE
+        id=$<id>
+      RETURNING *
+    `,
+      photographer
+    );
+  } catch (error) {
+    console.error(error);
+    throw "Internal server error.";
+  }
+}
+
+async function destroy(id) {
+  try {
+    return await db.any(
+      `
+      DELETE FROM 
+        photographers
+      WHERE
+        id=$1
+      RETURNING *
+    `,
+      id
+    );
+  } catch (error) {
+    console.error(error);
+    throw "Internal server error.";
+  }
+}
+
+module.exports = {
+  NAME: "Photographer",
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy,
+};
